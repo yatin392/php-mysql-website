ce6153e9c06bf57dce48c7d9b1260cddb1bb8ddb
yatin392 <55752206+yatin392@users.noreply.github.com>
2021-09-30 10:27:57 +0530

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,10 +2,10 @@
 
 session_start();
 
-$db_host = "yatin sharma";
-$db_user = "username";
-$db_pass = "password";
-$db_name = "database";
+$db_host = "mysql392.mysql.database.azure.com";
+$db_user = "sharma@mysql392";
+$db_pass = "yatin@yatin1";
+$db_name = "secondsql";
 
 $mysql = new mysqli($db_host, $db_user, $db_pass, $db_name);
 $mysql->set_charset("utf8");
